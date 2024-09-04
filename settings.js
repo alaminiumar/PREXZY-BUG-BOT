1b39844c5f4d6f054d9a65fc8e9c405d78afce63
alaminiumar <158068501+alaminiumar@users.noreply.github.com>
2024-09-04 17:11:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '2349136912476'  //creator number
+global.ownername = 'mr robot' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
